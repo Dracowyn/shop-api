90b79d595901884dd8a7740cded83496caee4b2f
Dracowyn <dracowyn@outlook.com>
2023-12-22 16:06:56 +0800

    实现立即购买功能

--- app/Http/Controllers/Shop/Product/OrderController.php
+++ app/Http/Controllers/Shop/Product/OrderController.php
@@ -17,6 +17,8 @@ use App\Models\Business\Business as BusinessModel;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class OrderController extends ShopController
 {
@@ -26,35 +28,210 @@ class OrderController extends ShopController
         $params = request()->input();
         $business = request()->get('business');
 
-        $cartIds = isset($params['cartids']) ? explode(',', $params['cartids']) : [];
-
-        if ($cartIds == [] && !$cartIds && !is_array($cartIds)) {
-            return $this->error('购物车数据不存在', null);
-        }
-
-        $cartData = CartModel::with('product')->whereIn('id', $cartIds)->get();
-
-        if (!$cartData->toArray()) {
-            return $this->error('购物车数据不存在', null);
-        }
-
         $address = AddressModel::find($params['addressid']);
 
         if (!$address) {
             return $this->error('收货地址不存在', null);
         }
 
+
+
         // 订单总额
         $total = 0;
 
-        foreach ($cartData as $item) {
-            $stock = bcsub($item->product->stock, $item->nums);
+        if (isset($params['proid'])) {
+            return $this->extracted();
+        } else {
+            $cartIds = isset($params['cartids']) ? explode(',', $params['cartids']) : [];
+            if ($cartIds == [] && !$cartIds && !is_array($cartIds)) {
+                return $this->error('购物车数据不存在', null);
+            }
+            $cartData = CartModel::with('product')->whereIn('id', $cartIds)->get();
 
-            if ($stock < 0) {
-                return $this->error('商品库存不足', null);
+            if (!$cartData->toArray()) {
+                return $this->error('购物车数据不存在', null);
             }
 
-            $total += $item->total;
+            foreach ($cartData as $item) {
+                $stock = bcsub($item->product->stock, $item->nums);
+
+                if ($stock < 0) {
+                    return $this->error('商品库存不足', null);
+                }
+
+                $total += $item->total;
+            }
+
+            // 开启事务
+            DB::beginTransaction();
+
+            // 创建订单
+            $orderData = [
+                'code' => build_order('BU'),
+                'busid' => $business->id,
+                'businessaddrid' => $address->id,
+                'amount' => $total,
+                'remark' => $params['content'] ?? null,
+                'status' => "0"
+            ];
+
+            $validate = [
+                [
+                    'busid' => 'required',
+                    'businessaddrid' => 'required',
+                    'code' => 'required|unique:order',
+                    'status' => 'in:0,1,2,3,4',
+                    'amount' => 'required'
+                ],
+                [
+                    'busid.required' => '用户信息未知',
+                    'businessaddrid.required' => '收货地址未知',
+                    'code.required' => '订单号必填',
+                    'code.unique' => '订单号已存在，请重新输入',
+                    'amount.required' => '订单号金额未知',
+                ]
+            ];
+
+            $validator = Validator::make($orderData, ...$validate);
+
+            if ($validator->fails()) {
+                return $this->error($validator->errors()->first(), null);
+            }
+
+            $orderStatus = OrderModel::create($orderData);
+
+            if ($orderStatus === false) {
+                DB::rollBack();
+                return $this->error('订单创建失败', null);
+            }
+
+            $orderProductData = [];
+
+            foreach ($cartData as $item) {
+                $orderProductData[] = [
+                    'orderid' => $orderStatus->id,
+                    'proid' => $item->product->id,
+                    'nums' => $item->nums,
+                    'price' => $item->product->price,
+                    'total' => $item->total,
+                ];
+            }
+
+            $orderProductValidate = [
+                [
+                    'orderid' => 'required', //必填
+                    'proid' => 'required', //必填
+                    'nums' => 'required|gt:0', //必填
+                    'price' => 'required|gt:0', //必填
+                    'total' => 'required|gt:0', //必填
+                ],
+                [
+                    'orderid.required' => '订单ID未知',
+                    'proid.required' => '商品ID未知',
+                    'nums.required' => '请填写商品数量',
+                    'price.required' => '请填写商品的单价',
+                    'total.required' => '请填写商品的总价',
+                    'nums.gt' => '商品数量大于0',
+                    'price.gt' => '商品的单价大于0',
+                    'total.gt' => '商品的总价大于0',
+                ]
+            ];
+
+            foreach ($orderProductData as $item) {
+                $orderProductValidator = Validator::make($item, ...$orderProductValidate);
+                if ($orderProductValidator->fails()) {
+                    return $this->error($orderProductValidator->errors()->first(), null);
+                }
+            }
+
+            $orderProductStatus = ProductOrderModel::insert($orderProductData);
+
+            // 更新商品库存
+            $productData = [];
+            foreach ($cartData as $item) {
+                $productData[] = [
+                    'id' => $item->product->id,
+                    'stock' => bcsub($item->product->stock, $item->nums),
+                ];
+            }
+
+            $productStatus = ProductModel::upsert($productData, ['id'], ['stock']);
+
+            // 删除购物车数据
+            $cartStatus = CartModel::whereIn('id', $cartIds)->delete();
+
+            if ($orderProductStatus === false || $productStatus === false || $cartStatus === false) {
+                DB::rollBack();
+                return $this->error('订单创建失败', null);
+            } else {
+                DB::commit();
+                return $this->success('订单创建成功', null);
+            }
+        }
+    }
+
+    // 订单列表
+    public function index(): JsonResponse
+    {
+        $business = request()->get('business');
+
+        $page = request('page', 1);
+        $limit = request('limit', 10);
+
+        $start = ($page - 1) * $limit;
+
+        $orderData = OrderModel::where(['busid' => $business->id])->offset($start)->limit($limit)->get('id,code,amount,remark,status,created_at');
+
+        $orderCount = OrderModel::where(['busid' => $business->id])->count();
+
+        $data = [
+            'count' => $orderCount,
+            'list' => $orderData,
+        ];
+
+        if ($orderData) {
+            return $this->success('获取成功', $data);
+        } else {
+            return $this->error('暂无订单', []);
+        }
+    }
+
+    public function productOrder(): JsonResponse
+    {
+        return $this->extracted();
+    }
+
+    /**
+     * 提取的方法
+     * @return JsonResponse
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function extracted(): JsonResponse
+    {
+        $params = request()->input();
+        $business = request()->get('business');
+
+        $proId = $params['proid'] ?? 0;
+
+        $productData = ProductModel::find($proId);
+
+        if (!$productData) {
+            return $this->error('商品不存在', null);
+        }
+        // 订单总额
+        $total = 0;
+
+        // 判断商品库存
+        $stock = bcsub($productData->stock, $params['nums']);
+        if ($stock <= 0) {
+            return $this->error('商品库存不足', null);
+        }
+
+        $address = AddressModel::find($params['addressid']);
+
+        if (!$address) {
+            return $this->error('收货地址不存在', null);
         }
 
         // 开启事务
@@ -100,17 +277,13 @@ class OrderController extends ShopController
             return $this->error('订单创建失败', null);
         }
 
-        $orderProductData = [];
-
-        foreach ($cartData as $item) {
-            $orderProductData[] = [
-                'orderid' => $orderStatus->id,
-                'proid' => $item->product->id,
-                'nums' => $item->nums,
-                'price' => $item->product->price,
-                'total' => $item->total,
-            ];
-        }
+        $orderProductData = [
+            'orderid' => $orderStatus->id,
+            'proid' => $productData->id,
+            'nums' => $params['nums'],
+            'price' => $productData->price,
+            'total' => bcmul($params['nums'], $productData->price, 2),
+        ];
 
         $orderProductValidate = [
             [
@@ -132,61 +305,29 @@ class OrderController extends ShopController
             ]
         ];
 
-        foreach ($orderProductData as $item) {
-            $orderProductValidator = Validator::make($item, ...$orderProductValidate);
-            if ($orderProductValidator->fails()) {
-                return $this->error($orderProductValidator->errors()->first(), null);
-            }
+
+        $orderProductValidator = Validator::make($orderProductData, ...$orderProductValidate);
+        if ($orderProductValidator->fails()) {
+            return $this->error($orderProductValidator->errors()->first(), null);
         }
 
         $orderProductStatus = ProductOrderModel::insert($orderProductData);
 
         // 更新商品库存
-        $productData = [];
-        foreach ($cartData as $item) {
-            $productData[] = [
-                'id' => $item->product->id,
-                'stock' => bcsub($item->product->stock, $item->nums),
-            ];
-        }
+        $productData = [
+            'id' => $productData->id,
+            'stock' => $stock,
+        ];
 
         $productStatus = ProductModel::upsert($productData, ['id'], ['stock']);
 
-        // 删除购物车数据
-        $cartStatus = CartModel::whereIn('id', $cartIds)->delete();
 
-        if ($orderProductStatus === false || $productStatus === false || $cartStatus === false) {
+        if ($orderProductStatus === false || $productStatus === false) {
             DB::rollBack();
             return $this->error('订单创建失败', null);
         } else {
             DB::commit();
             return $this->success('订单创建成功', null);
-        }
-    }
-
-    // 订单列表
-    public function index(): JsonResponse
-    {
-        $business = request()->get('business');
-
-        $page = request('page', 1);
-        $limit = request('limit', 10);
-
-        $start = ($page - 1) * $limit;
-
-        $orderData = OrderModel::where(['busid' => $business->id])->offset($start)->limit($limit)->get('id,code,amount,remark,status,created_at');
-
-        $orderCount = OrderModel::where(['busid' => $business->id])->count();
-
-        $data = [
-            'count' => $orderCount,
-            'list' => $orderData,
-        ];
-
-        if ($orderData) {
-            return $this->success('获取成功', $data);
-        } else {
-            return $this->error('暂无订单', []);
         }
     }
 }
