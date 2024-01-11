90973c858f723f78f6db992e3c1a5c39bd759696
Dracowyn <dracowyn@outlook.com>
2024-01-11 17:08:34 +0800

    新增Category模型

---
+++ app/Models/Category.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @author Dracowyn
+ * @since 2024-01-11 17:03
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Config as ConfigModel;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'category';
+
+    public $timestamps = true;
+
+    protected $dateFormat = 'U';
+
+    const CREATED_AT = 'createtime';
+
+    const UPDATED_AT = 'updatetime';
+
+    protected $guarded = [];
+
+    protected $appends = [
+        'type_text',
+        'flag_text'
+    ];
+
+    public function getFlagList(): array
+    {
+        return ['hot' => '热门', 'new' => '新品', 'recommend' => '推荐'];
+    }
+
+    public function getFlagTextAttribute(): string
+    {
+        $value = explode(',', $this->flag);
+        $flag = $this->getFlagList();
+        return implode(',', array_intersect_key($flag, array_flip($value)));
+    }
+
+    public function getTypeTextAttribute()
+    {
+        $typeJson = ConfigModel::where('name', 'category_type')->value('value');
+
+        $type = json_decode($typeJson, true);
+
+        return $type[$this->type] ?? '';
+    }
+
+
+}
